ce20ad65d3689806868f28ea5014b2a3cd211c17
meow <3097573956@qq.com>
2026-05-10 17:39:16 +0800

    build bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -256,7 +256,7 @@ export default defineConfig({
 		resolve: {
 			alias: {
 				"@rehype-callouts-theme": `rehype-callouts/theme/${siteConfig.rehypeCallouts.theme}`,
-				'@i18n': '/src/i18n',
+				'@i18n': './src/i18n',
 			},
 		},
 		build: {
